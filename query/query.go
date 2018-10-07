aaf93935263b8d38790de7d4bcaff5247e0fac3d
Ulrik Holmen <ulrik.holmen@nttsecurity.com>
2018-10-07 12:14:28 +0000

    Fixed typo in a[1] vs a[0] in before/after criteria

--- query/query.go
+++ query/query.go
@@ -179,10 +179,10 @@ func (a timeQuery) LookupIn(ctx context.Context, index *indexfile.IndexFile) (bp
 	t := time.Unix(0, intval*1000) // converts micros -> nanos
 	// Additional debugging to find the cause of "missed skips"
 	if !a[0].IsZero() {
-		v(2, "After criteria is set to %v which is %s in epoch", a[1].Format(time.RFC3339), strconv.FormatInt(a[1].Unix(), 10))
+		v(2, "After criteria is set to %v which is %s in epoch", a[0].Format(time.RFC3339), strconv.FormatInt(a[0].Unix(), 10))
 	}
 	if !a[1].IsZero() {
-		v(2, "Before criteria is set to %v which is %s in epoch", a[0].Format(time.RFC3339), strconv.FormatInt(a[0].Unix(), 10))
+		v(2, "Before criteria is set to %v which is %s in epoch", a[1].Format(time.RFC3339), strconv.FormatInt(a[1].Unix(), 10))
 	}
 	// Note, we add a minute when doing 'before' queries and subtract a minute
 	// when doing 'after' queries, to make sure we actually get the time
